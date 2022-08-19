4f2bdb96ddabfb2e982a6e86da6220f26e7dd6d1
bradenmweight <bweight@ur.rochester.edu>
2022-08-19 12:55:27 -0400

    Added multiple diabatic population outputs

--- get_ave_FSSH.py
+++ get_ave_FSSH.py
@@ -2,39 +2,64 @@ import numpy as np
 #from matplotlib import pyplot as plt
 import math
 
-NTraj = 10 ** 3
-NStates = 3
+NTraj = 10 ** 4
 
-time           = np.loadtxt("traj-0/active_state.dat")[:,[0,1]]
-NSteps         = len( time )
-coeffs_pop_ad  = np.zeros(( NSteps, NStates ))
-coeffs_pop_dia = np.zeros(( NSteps, NStates ))
-active_state   = np.zeros(( NSteps, NStates ))
+time               = np.loadtxt("traj-0/active_state.dat")[:,[0,1]]
+NStates            = len(np.loadtxt("traj-0/active_state.dat")[0,2:])
+NSteps             = len( time )
+coeffs_pop_ad      = np.zeros(( NSteps, NStates ))
+coeffs_pop_dia_0   = np.zeros(( NSteps, NStates ))
+coeffs_pop_dia_1   = np.zeros(( NSteps, NStates ))
+coeffs_pop_dia_2   = np.zeros(( NSteps, NStates ))
+active_state       = np.zeros(( NSteps, NStates ))
 
-
-def active_state_populations( integers, active_state ):
-    for step in range( NSteps ):
-        index = integers[step]
-        #print( step, index, active_state[step, :] )
-        active_state[step,index]  += 1
-    return active_state
 
 for traj in range( NTraj ):
     print( f"traj {traj} of {NTraj}" )
-    #integers          = np.loadtxt(f"traj-{traj}/active_state.dat")[:,2].astype(int)
-    #active_state      = active_state_populations( integers, active_state )
-    active_state     += np.loadtxt(f"traj-{traj}/active_state.dat")[:,2:]
-    coeffs_pop_ad    += np.loadtxt(f"traj-{traj}/density_dia_re.dat")[:,[2,6,10]]
-    coeffs_pop_dia   += np.loadtxt(f"traj-{traj}/density_ad_re.dat")[:,[2,6,10]]
+    try:
+        active_state     += np.loadtxt(f"traj-{traj}/active_state.dat")[:,2:]
+    except OSError:
+        NTraj = traj
+        break
+    if ( NStates == 2 ):
+        coeffs_pop_ad    += np.loadtxt(f"traj-{traj}/density_ad_re.dat")[:,[2,5]]
+        coeffs_pop_dia_0   += np.loadtxt(f"traj-{traj}/density_dia_re_0.dat")[:,[2,5]]        
+        coeffs_pop_dia_1   += np.loadtxt(f"traj-{traj}/density_dia_re_1.dat")[:,[2,5]]        
+        coeffs_pop_dia_2   += np.loadtxt(f"traj-{traj}/density_dia_re_2.dat")[:,[2,5]]        
+    elif ( NStates == 3 ):
+        coeffs_pop_ad    += np.loadtxt(f"traj-{traj}/density_ad_re.dat")[:,[2,6,10]]
+        coeffs_pop_dia_0   += np.loadtxt(f"traj-{traj}/density_dia_re_0.dat")[:,[2,6,10]]
+        coeffs_pop_dia_1   += np.loadtxt(f"traj-{traj}/density_dia_re_1.dat")[:,[2,6,10]]
+        coeffs_pop_dia_2   += np.loadtxt(f"traj-{traj}/density_dia_re_2.dat")[:,[2,6,10]]
 
-
-#for step in range( NSteps ):
-    #active_state[step,:] /= np.sum( active_state[step,:] )
+print(f"There were {NTraj} of {NTraj} good trajectories.")
 active_state    /= NTraj
 coeffs_pop_ad   /= NTraj
-coeffs_pop_dia  /= NTraj
+coeffs_pop_dia_0  /= NTraj
+coeffs_pop_dia_1  /= NTraj
+coeffs_pop_dia_2  /= NTraj
 
-np.savetxt("POP_AS.dat", np.c_[ time[:,0], time[:,1], active_state[:,0], active_state[:,1], active_state[:,2] ], fmt="%1.5f")
-np.savetxt("POP_COEFF_AD.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_ad[:,0], coeffs_pop_ad[:,1], coeffs_pop_ad[:,2] ], fmt="%1.5f")
-np.savetxt("POP_COEFF_DIA.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia[:,0], coeffs_pop_dia[:,1], coeffs_pop_dia[:,2] ], fmt="%1.5f")
+
+if ( NStates == 2 ):
+    np.savetxt("POP_AS.dat", np.c_[ time[:,0], time[:,1], active_state[:,0], active_state[:,1] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_AD.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_ad[:,0], coeffs_pop_ad[:,1] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_0.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_0[:,0], coeffs_pop_dia_0[:,1] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_1.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_1[:,0], coeffs_pop_dia_1[:,1] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_2.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_2[:,0], coeffs_pop_dia_2[:,1] ], fmt="%1.5f")
+       
+elif ( NStates == 3 ):
+    np.savetxt("POP_AS.dat", np.c_[ time[:,0], time[:,1], active_state[:,0], active_state[:,1], active_state[:,2] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_AD.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_ad[:,0], coeffs_pop_ad[:,1], coeffs_pop_ad[:,2] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_0.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_0[:,0], coeffs_pop_dia_0[:,1], coeffs_pop_dia_0[:,2] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_1.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_1[:,0], coeffs_pop_dia_1[:,1], coeffs_pop_dia_1[:,2] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA_2.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_2[:,0], coeffs_pop_dia_2[:,1], coeffs_pop_dia_2[:,2] ], fmt="%1.5f")
+
+
+
+
+
+
+
+
+
 
