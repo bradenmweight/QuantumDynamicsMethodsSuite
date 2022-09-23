Bug fixes and update FSSH average

MQC/FSSH.py
@@ -13,7 +13,7 @@ def getGlobalParams():
     global dtE, dtI, NSteps, NTraj, NStates, M
     global NCPUS, initState, dirName
     global fs_to_au, NSkip, NDOF
-    global rescale_type, decoherece_type
+    global rescale_type, decoherece_type, SWAP_COEFFS_HOP
     global Diabatic_Density_Type, EDC_PARAM, AS_POP_INCREASE
     dtE = model.parameters.dtE
     dtI = model.parameters.dtI
@@ -27,6 +27,7 @@ def getGlobalParams():
     rescale_type = model.parameters.rescale_type
     NSkip = model.parameters.NSkip
     NDOF = model.parameters.NDOF
+    SWAP_COEFFS_HOP = model.parameters.SWAP_COEFFS_HOP
     decoherece_type = model.parameters.decoherece_type
     Diabatic_Density_Type = model.parameters.Diabatic_Density_Type
     EDC_PARAM = model.parameters.EDC_PARAM
@@ -491,15 +492,15 @@ def evaluate_hop( active_state, hop_data, M, V, Ead, deriv_coup, z_ad ):
                 V[:] -= scale_factor * deriv_coup[start_state,end_state,:] / M[:]
 
 
-
         # DEPING SUGGESTS TO TURN OFF STATE SWAPPING
         # Rearrange states based on hop
+        if ( SWAP_COEFFS_HOP == 1 ):
+            tmp = np.copy( z_ad[start_state] )
+            z_ad[start_state] = z_ad[end_state]
+            z_ad[end_state] = tmp
+
         active_state = end_state
-        """
-        tmp = np.copy( z_ad[start_state] )
-        z_ad[start_state] = z_ad[end_state]
-        z_ad[end_state] = tmp
-        """
+
 
     return M*V, z_ad, active_state
 
@@ -554,11 +555,8 @@ def VelVerF(R, P, z_ad, Uad_old, active_state, RFile, VFile, HelFile, energyFile
     F1 = Force(dHel_ad, R, z_ad, active_state, step, dHel0, forceFile, writeForce=True)
 
     P += 0.5000 * F1 * dtI # Half-step velocity
-    ##########P = np.array([ 10000 ])
 
     R += P / M * dtI # Full Step Position
-    #R = np.array([2.100 + (step+1) * 0.0025])
-    #R = np.array([ 3.0 ])
 
     Hel = model.Hel(R) # Electronic Structure
     Ead, Uad = np.linalg.eigh(Hel) # Return adiabatic states and transformation matrices
@@ -591,7 +589,6 @@ def RunIterations(n): # This is parallelized already. "Main" for each trajectory
     activeStateFile, energyFileAd, energyFileDia, densityFiles, InitCondsFile, RFile, VFile, HelFile, probFile, randFile, NACRFile, NACTFile, forceFile = initFiles(n) # Makes file objects
 
     R,P = model.initR() # Initialize nuclear DOF
-    #R,P = np.array([2.1]), np.array([0.0])
 
     z, active_state = initCoeffs(InitCondsFile)
     Uad = np.identity( NStates )

helper_files/get_FSSH_results.py
@@ -2,15 +2,13 @@ import numpy as np
 #from matplotlib import pyplot as plt
 import math
 
-NTraj = 10 ** 4
+NTraj = 10 ** 5
 
 time               = np.loadtxt("traj-0/active_state.dat")[:,[0,1]]
 NStates            = len(np.loadtxt("traj-0/active_state.dat")[0,2:])
 NSteps             = len( time )
 coeffs_pop_ad      = np.zeros(( NSteps, NStates ))
-coeffs_pop_dia_0   = np.zeros(( NSteps, NStates ))
-coeffs_pop_dia_1   = np.zeros(( NSteps, NStates ))
-coeffs_pop_dia_2   = np.zeros(( NSteps, NStates ))
+coeffs_pop_dia     = np.zeros(( NSteps, NStates ))
 active_state       = np.zeros(( NSteps, NStates ))
 
 
@@ -23,36 +21,25 @@ for traj in range( NTraj ):
         break
     if ( NStates == 2 ):
         coeffs_pop_ad    += np.loadtxt(f"traj-{traj}/density_ad_re.dat")[:,[2,5]]
-        coeffs_pop_dia_0   += np.loadtxt(f"traj-{traj}/density_dia_re_0.dat")[:,[2,5]]        
-        coeffs_pop_dia_1   += np.loadtxt(f"traj-{traj}/density_dia_re_1.dat")[:,[2,5]]        
-        coeffs_pop_dia_2   += np.loadtxt(f"traj-{traj}/density_dia_re_2.dat")[:,[2,5]]        
+        coeffs_pop_dia   += np.loadtxt(f"traj-{traj}/density_dia_re.dat")[:,[2,5]]            
     elif ( NStates == 3 ):
         coeffs_pop_ad    += np.loadtxt(f"traj-{traj}/density_ad_re.dat")[:,[2,6,10]]
-        coeffs_pop_dia_0   += np.loadtxt(f"traj-{traj}/density_dia_re_0.dat")[:,[2,6,10]]
-        coeffs_pop_dia_1   += np.loadtxt(f"traj-{traj}/density_dia_re_1.dat")[:,[2,6,10]]
-        coeffs_pop_dia_2   += np.loadtxt(f"traj-{traj}/density_dia_re_2.dat")[:,[2,6,10]]
+        coeffs_pop_dia   += np.loadtxt(f"traj-{traj}/density_dia_re.dat")[:,[2,6,10]]
 
 print(f"There were {NTraj} of {NTraj} good trajectories.")
 active_state    /= NTraj
 coeffs_pop_ad   /= NTraj
-coeffs_pop_dia_0  /= NTraj
-coeffs_pop_dia_1  /= NTraj
-coeffs_pop_dia_2  /= NTraj
+coeffs_pop_dia  /= NTraj
 
 
 if ( NStates == 2 ):
     np.savetxt("POP_AS.dat", np.c_[ time[:,0], time[:,1], active_state[:,0], active_state[:,1] ], fmt="%1.5f")
     np.savetxt("POP_COEFF_AD.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_ad[:,0], coeffs_pop_ad[:,1] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_0.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_0[:,0], coeffs_pop_dia_0[:,1] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_1.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_1[:,0], coeffs_pop_dia_1[:,1] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_2.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_2[:,0], coeffs_pop_dia_2[:,1] ], fmt="%1.5f")
-       
+    np.savetxt("POP_COEFF_DIA.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia[:,0], coeffs_pop_dia[:,1] ], fmt="%1.5f")
 elif ( NStates == 3 ):
     np.savetxt("POP_AS.dat", np.c_[ time[:,0], time[:,1], active_state[:,0], active_state[:,1], active_state[:,2] ], fmt="%1.5f")
     np.savetxt("POP_COEFF_AD.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_ad[:,0], coeffs_pop_ad[:,1], coeffs_pop_ad[:,2] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_0.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_0[:,0], coeffs_pop_dia_0[:,1], coeffs_pop_dia_0[:,2] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_1.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_1[:,0], coeffs_pop_dia_1[:,1], coeffs_pop_dia_1[:,2] ], fmt="%1.5f")
-    np.savetxt("POP_COEFF_DIA_2.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia_2[:,0], coeffs_pop_dia_2[:,1], coeffs_pop_dia_2[:,2] ], fmt="%1.5f")
+    np.savetxt("POP_COEFF_DIA.dat", np.c_[ time[:,0], time[:,1], coeffs_pop_dia[:,0], coeffs_pop_dia[:,1], coeffs_pop_dia[:,2] ], fmt="%1.5f")
 
 
 